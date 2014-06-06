a38fdc6b1cb488f5bbd0f4686f48865ea5ac6574
Adam Byram <abyram@gmail.com>
2014-06-06 14:52:16 -0400

    Adds basic mongodb support w/ seed data

---
+++ data/database.js
@@ -0,0 +1,22 @@
+(function(database) {
+    var mongodb = require('mongodb');
+    var mongoUrl = 'mongodb://localhost:27017/confstack';
+    var theDb = null;
+    database.getDb = function (next) {
+        if(!theDb) {
+            mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+               if(err) {
+                   next(err);
+               } else {
+                   theDb = {
+                       db: db,
+                       sessions: db.collection('sessions')
+                   };
+                   next(null, theDb);
+               }
+            });
+        } else {
+            next(null, theDb);
+        }
+    };
+})(module.exports);
--- data/index.js
+++ data/index.js
@@ -1,6 +1,50 @@
 (function(data){
     var seedData = require('./seedData');
+    var database = require('./database');
+
     data.getSessions = function (next) {
-      next(null, seedData.sessionList);
+        database.getDb(function(err, db) {
+            if(err) {
+                next(err, null);
+            } else {
+                db.sessions.find({speaker: 'Adam Byram'}).toArray(function (err, results) {
+                    if(err) {
+                        next(err, null);
+                    } else {
+                        next(null, results);
+                    }
+                });
+            }
+        });
+
     };
+
+    function seedDatabase() {
+        database.getDb(function(err, db) {
+           if(err) {
+               console.log('Failed to seed database: ' + err);
+           } else {
+                db.sessions.count(function(err, count) {
+                   if(err) {
+                       console.log('Failed to get database count: ' + err);
+                   } else {
+                       if(count === 0) {
+                           console.log('Seeding the database...');
+                           seedData.sessionList.forEach(function(session) {
+                              db.sessions.insert(session, function(err) {
+                                 if(err) {
+                                     console.log('Failed to insert data in seed database: ' + err);
+                                 }
+                              });
+                           });
+                       } else {
+                           console.log('Database already seeded.');
+                       }
+                   }
+                });
+           }
+        });
+    };
+
+    seedDatabase();
 })(module.exports);
