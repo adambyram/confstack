3fd18146e2e8440b4c3f4d86aff9e23d4f9859b9
Adam Byram <abyram@gmail.com>
2014-06-06 11:04:24 -0400

    Bare bones Express install

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+   res.send('<html><body><h1>' + req.url + '</h1></body></html>');
+});
+
+var server = http.createServer(app);
+server.listen(3000);
